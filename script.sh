d03756da0427a8f1aa3d328a2b44cd19adb2d081
João Marcos Bezerra <marcospb19@hotmail.com>
2020-11-15 15:30:24 -0300

    Adding check for $1 argument

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ ! "$1" ]]; then
+    echo "You need to specify the .cpp file"
+    exit 1 # Error exit code
+fi
+
 x=$(g++ -std=c++11 $1 -H 2>&1 | grep bits/stdc++.h)
 directory=`echo $x | awk '{ print $NF }'`
 
